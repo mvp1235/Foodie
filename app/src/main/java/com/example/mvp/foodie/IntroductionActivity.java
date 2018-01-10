f3b882e7af93e692d7fb1549f3a25b204860a61e
Huy Nguyen <h.nguyen1235@gmail.com>
2018-01-10 11:00:46 -0800

    Initial commit

---
+++ app/src/main/java/com/example/mvp/foodie/IntroductionActivity.java
@@ -0,0 +1,36 @@
+package com.example.mvp.foodie;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+
+public class IntroductionActivity extends AppCompatActivity {
+
+    private AppCompatButton signInBtn, signUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+
+        signInBtn = findViewById(R.id.signInBtn_id);
+        signUpBtn = findViewById(R.id.signUpBtn_id);
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroductionActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
