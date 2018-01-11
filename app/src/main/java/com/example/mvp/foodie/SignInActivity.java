e188f93faa253db37a2c24a69bc0f8dab8832cb3
Huy Nguyen <h.nguyen1235@gmail.com>
2018-01-11 10:53:00 -0800

    added functionality for profile picture on sign up screen

--- app/src/main/java/com/example/mvp/foodie/SignInActivity.java
+++ app/src/main/java/com/example/mvp/foodie/SignInActivity.java
@@ -1,13 +1,45 @@
 package com.example.mvp.foodie;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.View;
 
 public class SignInActivity extends AppCompatActivity {
+
+    final static int CREATE_ACCOUNT_CODE = 100;
+
+    AppCompatButton facebookLoginBtn, googleLoginBtn, loginBtn, createAccountBtn, forgotPasswordBtn;
+    AppCompatEditText usernameET, passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        //Referencing the buttons
+        facebookLoginBtn = findViewById(R.id.facebookLogin_id);
+        googleLoginBtn = findViewById(R.id.googleLogin_id);
+        loginBtn = findViewById(R.id.loginBtn_id);
+        createAccountBtn = findViewById(R.id.createAccountBtn_id);
+        forgotPasswordBtn = findViewById(R.id.forgotPasswordBtn_id);
+
+        //Referencing the EditTexts
+        usernameET = findViewById(R.id.usernameText_id);
+        passwordET = findViewById(R.id.passwordText_id);
+
+        //Setting onclick listener for the buttons
+        createAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivityForResult(intent, CREATE_ACCOUNT_CODE);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/mvp/foodie/SignUpActivity.java
+++ app/src/main/java/com/example/mvp/foodie/SignUpActivity.java
@@ -1,13 +1,102 @@
 package com.example.mvp.foodie;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.ByteArrayOutputStream;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private final static int REQUEST_GALLERY_PHOTO = 200;
+    private final static int REQUEST_IMAGE_CAPTURE = 201;
+
+    ImageView profilePhotoIV;
+    AppCompatEditText firstNameET, lastNameET, emailET, passwordET;
+    AppCompatButton signUpBtn;
+
+    private AlertDialog photoActionDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        profilePhotoIV = findViewById(R.id.profilePhoto_id);
+
+        firstNameET = findViewById(R.id.firstNameText_id);
+        lastNameET = findViewById(R.id.lastNameText_id);
+        emailET = findViewById(R.id.emailAddressText_id);
+        passwordET = findViewById(R.id.passwordText_id);
+
+        signUpBtn = findViewById(R.id.signUpBtn_id);
+
+        profilePhotoIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPhotoActionDialog();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap parkingBitmap = (Bitmap) extras.get("data");
+
+            profilePhotoIV.setImageBitmap(parkingBitmap);
+            photoActionDialog.dismiss();
+
+        } else if (requestCode == REQUEST_GALLERY_PHOTO && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            profilePhotoIV.setImageURI(imageUri);
+            photoActionDialog.dismiss();
+
+        }
+    }
+
+    private void showPhotoActionDialog() {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(SignUpActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_pick_photos, null);
+        LinearLayout galleryLL = mView.findViewById(R.id.galleryLL);
+        LinearLayout cameraLL = mView.findViewById(R.id.cameraLL);
+
+        galleryLL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(intent, REQUEST_GALLERY_PHOTO);
+            }
+        });
+
+        cameraLL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        mBuilder.setView(mView);
+        photoActionDialog = mBuilder.create();
+        photoActionDialog.show();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
