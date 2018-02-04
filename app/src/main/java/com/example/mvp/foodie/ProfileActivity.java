0cff42f262c463b3b46bc177765cb69644583927
Huy Nguyen <h.nguyen1235@gmail.com>
2018-02-04 15:56:56 -0800

    partial design for profile page

--- app/src/main/java/com/example/mvp/foodie/ProfileActivity.java
+++ app/src/main/java/com/example/mvp/foodie/ProfileActivity.java
@@ -2,12 +2,43 @@ package com.example.mvp.foodie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        toolbar = findViewById(R.id.profile_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.profile);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_toolbar_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //Set action upon clicking on menu items
+        switch (id) {
+            case R.id.profile_edit:
+//                Toast.makeText(getApplicationContext(), "Editing Profile", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
