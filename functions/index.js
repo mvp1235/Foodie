5e6a844fd03b67efa806bc6e5c1f4f1397e4da38
Huy Nguyen <h.nguyen1235@gmail.com>
2018-04-21 11:52:09 -0700

    fix like notification, was displaying to user name instead of from

--- functions/index.js
+++ functions/index.js
@@ -5,22 +5,25 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.sendLikeNotifications = functions.database.ref('/Notifications/{user_id}/{notification_id}')
-	.onWrite((change, context) => {
-      const user_id = context.params.user_id;
+	.onCreate((snapshot, context) => {
+      const to_user_id = context.params.user_id;
+      const from_user_id = snapshot.val().fromUserID;
       const notification_id = context.params.notification_id;
       
-      if (!change.after.val()) {
-    	  return console.log('A notification has been deleted from the database: ', notification_id);
-      }
+//      if (!change.after.val()) {
+//    	  return console.log('A notification has been deleted from the database: ', notification_id);
+//      }
       
-      const toUser = admin.database().ref(`/Users/${user_id}`).once('value');
-      const deviceTokens = admin.database().ref(`/Users/${user_id}/tokenIDs`).once('value');
+      const toUser = admin.database().ref(`/Users/${to_user_id}`).once('value');
+      const deviceTokens = admin.database().ref(`/Users/${to_user_id}/tokenIDs`).once('value');
+      const fromUser = admin.database().ref(`/Users/${from_user_id}`).once('value');
       
-      return Promise.all([toUser, deviceTokens]).then(result => {
-    	  const from_user = result[0].val();
-    	  const token_ids = result[1].val();
+      return Promise.all([toUser, fromUser, deviceTokens]).then(result => {
+    	  const to_user = result[0].val();
+    	  const from_user = result[1].val();
+    	  const token_ids = result[2].val();
     	  
-    	  console.log('New like notification to user: ', from_user.uID);
+    	  console.log('New like notification to user: ', to_user.uID);
     	  
     	  if(!result[1].hasChildren()) {
     		  return console.log('There are no notification tokens to send to.');
